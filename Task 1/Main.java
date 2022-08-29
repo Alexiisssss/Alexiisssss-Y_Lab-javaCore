645a04e748aca68dea66c9876f5945bd7a0657e8
Alexey Volkov <75803784+Alexiisssss@users.noreply.github.com>
2022-08-30 00:32:09 +0500

    Add files via upload

---
+++ Task 1/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+  public static void main(String[] args) {
+    int n = 100;
+    int[] array = new int[n];
+    for (int i = 0; i < array.length; i++) {
+      array[i] = (int) (Math.random() * 1000);
+    }
+
+    int max = array[0];
+    int min = array[0];
+    int avg = 0;
+    for (int i = 0; i < array.length; i++) {
+      if (max < array[i]) {
+        max = array[i];
+      }
+      if (min > array[i]) {
+        min = array[i];
+      }
+      avg += array[i] / array.length;
+    }
+
+    System.out.println("max = " + max);
+    System.out.println("min = " + min);
+    System.out.println("avg = " + avg);
+
+  }
+}
